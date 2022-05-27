a10cc045fc02b21424117bc6c12a7539cbeb0192
jonathan pun <jpppun@gmail.com>
2022-05-27 14:14:50 +0800

    upgrade spring boot version add stop search api

--- src/main/kotlin/com/jonathanpun/hktransport/Controller.kt
+++ src/main/kotlin/com/jonathanpun/hktransport/Controller.kt
@@ -1,23 +1,36 @@
 package com.jonathanpun.hktransport
+import com.jonathanpun.hktransport.db.StopsRepository
 import com.jonathanpun.hktransport.repository.KMBRoute
 import com.jonathanpun.hktransport.repository.KMBRepository
+import com.jonathanpun.hktransport.repository.KMBStop
 import com.jonathanpun.hktransport.repository.KMBStopETA
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestParam
 
 
 @RestController
 class Controller{
     @Autowired
     lateinit var repository: KMBRepository
+    @Autowired
+    lateinit var stopsRepository: StopsRepository
     @GetMapping("/")
      suspend fun get(): List<KMBRoute>? {
         return repository.getAllRoute()
     }
     @GetMapping("/stops-eta/{stopId}")
-    suspend fun get(@PathVariable("stopId") stopId:String): List<KMBStopETA>? {
+    suspend fun getStopEta(@PathVariable("stopId") stopId:String): List<KMBStopETA>? {
         return repository.getStopEta(stopId)
+    }
+
+    @GetMapping("/stops")
+    suspend fun  getStop(@RequestParam(name = "q") query:String): List<KMBStop> {
+        println("-------------param---------------" + query)
+       return stopsRepository.findByNameEnContainingOrNameScContainingOrNameTcContaining(query,query,query)
     }
 }
--- src/main/kotlin/com/jonathanpun/hktransport/db/StopsRepository.kt
+++ src/main/kotlin/com/jonathanpun/hktransport/db/StopsRepository.kt
@@ -1,8 +1,11 @@
 package com.jonathanpun.hktransport.db
 
 import com.jonathanpun.hktransport.repository.KMBStop
+import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.repository.CrudRepository
 import org.springframework.stereotype.Component
 
 @Component
-interface StopsRepository:CrudRepository<KMBStop,String> {}
+interface StopsRepository:JpaRepository<KMBStop,String> {
+    fun findByNameEnContainingOrNameScContainingOrNameTcContaining(nameEn:String,nameSc:String,nameTc:String):List<KMBStop>
+}
